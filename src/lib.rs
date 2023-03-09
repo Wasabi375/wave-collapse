f96fbdcf775a770d2679ec37d0166b0d693b312f
Burkhard Mittelbach <wasabi37a@googlemail.com>
2023-03-09 21:18:44 +0100

    Add feature flag for tile2d
    In the future I will also want different feature flags, e.g. tile3d,
    maybe a graph andor other shapes with default implementations.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ pub mod binary_heap_set;
 pub mod error;
 pub mod gen_iter_return_result;
 pub mod node;
-pub mod tile2d;
 pub mod wave_function;
+
+#[cfg(feature = "tile2d")]
+pub mod tile2d;
 
 pub use gen_iter_return_result::GenIterReturnResult;
 pub use wave_function::collapse_wave;
